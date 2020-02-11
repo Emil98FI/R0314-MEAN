0523ec92bf81952cc8e43444cd04fb8a7ae77ef3
Emil98FI <ebrummer32@gmail.com>
2020-02-11 23:40:44 +0200

    Steezz

--- WS2/routeswithactions.js
+++ WS2/routeswithactions.js
@@ -9,6 +9,7 @@ var server = http.createServer(function(req, res) {
   var myStream = fs.createReadStream(__dirname + "/frontpage.html", "utf8");
   var myStreamC = fs.createReadStream(__dirname + "/contact.html", "utf8");
   var myStreamP = fs.createReadStream(__dirname + "/plaintext.txt");
+  var myStreamJ = fs.createReadStream(__dirname + "/sampledata.json");
 
   if (url === "/frontpage") {
     res.writeHead(200, { "Content-Type": "text/html" });
@@ -18,6 +19,9 @@ var server = http.createServer(function(req, res) {
     myStreamC.pipe(res);
   } else if (url === "/plaintext") {
     myStreamP.pipe(res);
+  } else if (url === "/json") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    myStreamJ.pipe(res);
   } else {
     res.write("Nothing to see here");
     res.end();
