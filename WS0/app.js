1baa6fcb35bbad90773eb8d3bb518aa68ccefd7e
Emil98FI <ebrummer32@gmail.com>
2020-02-11 15:18:04 +0200

    commit

--- WS0/app.js
+++ WS0/app.js
@@ -1,19 +1,18 @@
-var http=require('http');
+var http = require("http");
 
-var server=http.createServer(function(req,res){
-    
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write('<h1>Hello Node</h1>');
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello Node</h1>");
 
-    var data =
-      "<table border='1'><tr><td>Henri</td><td>Mikko</td></tr><tr><td>voitto</td><td>häviö</td></tr></table>";
-    res.write(data);
+  var data =
+    "<table border='1'><tr><td>Henr</td><td>Mikko</td></tr><tr><td>voitto</td><td>häviö</td></tr></table>";
+  res.write(data);
 
-    res.end();
+  res.end();
 });
 
-server.on('listening',function(){
-    console.log('ok, server is running');
+server.on("listening", function() {
+  console.log("ok, server is running");
 });
 
 server.listen(88);
