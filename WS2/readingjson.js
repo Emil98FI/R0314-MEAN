677b17792a44a8967e8746c92ab13abd629930bd
Emil98FI <ebrummer32@gmail.com>
2020-02-20 23:44:05 +0200

    NEW

--- WS2/readingjson.js
+++ WS2/readingjson.js
@@ -1,5 +1,24 @@
 var data = require(__dirname + "/sampledata.json");
 
+delete data[0];
+
+
+
 for (var i = 0; i < data.length; i++) {
+    console.log(data[i]);
+}
+
+data.push({
+
+    "name": "Joe doe",
+    "Age": "52",
+    "Company": "Laurea",
+    "address": "Ratatie"
+
+
+})
+
+for (var i = 0; i < data.length; i++) {
+
     console.log(data[i]);
 }
